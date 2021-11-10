e12fc0455c02a02c0cfcb479c311d3ecf3a8f645
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-11-10 13:50:16 -0500

    added the user birthday info, as well as css

---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = () =>{
+  return(
+    <>
+    <h2>List</h2>
+    </>
+  );
+};
+
+export default List;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: 1,
+    name: 'Johnny Bravo',
+    age: 45,
+  },
+  {
+     id: 2,
+    name: 'Michael Scott',
+    age: 53,
+
+  },
+   {
+     id: 3,
+    name: 'Lola Jenkins',
+    age: 25,
+
+  },
+   {
+     id: 4,
+    name: 'Michelle Scott',
+    age: 51,
+
+  },
+   {
+     id: 5,
+    name: 'Dan Joseph',
+    age: 22,
+
+  },
+]
