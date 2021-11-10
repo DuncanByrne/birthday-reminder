6ca1c0d0402a1a09debcb50836866a9b9c5d439f
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-11-10 15:19:06 -0500

    changed css and added pictures for the people

--- src/data.js
+++ src/data.js
@@ -1,31 +1,42 @@
 export default [
   {
     id: 1,
-    name: 'Johnny Bravo',
-    age: 45,
+    name: 'Julia',
+    age: 25,
+      image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
      id: 2,
-    name: 'Michael Scott',
-    age: 53,
+    name: 'Michelle',
+    age: 35,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
 
   },
    {
      id: 3,
-    name: 'Lola Jenkins',
-    age: 25,
+    name: 'Sean',
+    age: 28,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
 
   },
    {
      id: 4,
-    name: 'Michelle Obama',
-    age: 57,
+    name: 'Mike',
+    age: 19,
+     image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
 
   },
    {
      id: 5,
-    name: 'Dan Joseph',
+    name: 'Danielle',
     age: 22,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+
 
   },
 ];
